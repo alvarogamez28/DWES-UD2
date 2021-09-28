5af4a64e963a9dd52231faf4c7e40f5aaffc5870
alvarogamez28 <alvarogamjim2ciclo@gmail.com>
2021-09-28 08:43:32 +0200

    ejercicio1.php

---
+++ ejercicio1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nombre="Alvaro Gamez Jimenez";
+        $poblacion="Los Palacios y Vfca";
+        $edad="22";
+        printf("%s se encuentra en la poblacion de %s y su edad es %d años", $nombre,$poblacion,$edad);
+        print "</p>";
+        echo("$nombre se encuenta en la poblacion de $poblacion y su edad es $edad años");
+    ?>
+</body>
+</html>
